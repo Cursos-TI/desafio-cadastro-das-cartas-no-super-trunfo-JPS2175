99a8edec112ac2561822531264c0d970dcdd9f7c
JPS2175 <jacksontstchacal@gmail.com>
2025-05-06 17:44:08 +0000

    Criei o novo super_trunfo

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int main(){
+    int População;
+    float Área;
+    float PIB;
+    int Número;
+
+    printf("Didite sua População: \n");
+    scanf("%d", &População);
+
+    printf("Digite sua Área: \n");
+    ("%f", &Área);
+
+    printf("Digite seu PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Digite seu Número: \n");
+    scanf("%d", &Número);
+
+    printf("População: %d\nÁrea:%.2f km²\n", População, Área);
+    printf("PIB:%.2f bilhos de reais\nNúmeros de Pontos Turistico: %d\n", PIB, Número);
+
+
+    return 0;
+
+
+
+}
